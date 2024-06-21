881c97c8584084d0073d2c084aad51b9c8844316
Bibhuti Bhushan Saha <bibhuticodes@gmail.com>
2024-06-21 20:44:26 +0530

    removed boilerplate

---
+++ src/app/pages/home/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Contact from '../contact/page'
+
+export default function Home() {
+    return (
+        <div>
+            <Contact />
+            Home
+        </div>
+    )
+}
