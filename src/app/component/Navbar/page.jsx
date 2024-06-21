0d2c7b9407a2366c66181f9ad2ad86e4884fd768
Sarvesh025 <sarveshkumarroy1@gmail.com>
2024-06-21 22:56:20 +0530

    fixed navbar

--- src/app/component/Navbar/page.jsx
+++ src/app/component/Navbar/page.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './navbar.module.css'
+import Link from 'next/link'
 export default function Navbar() {
   return (
     <div className={styles.navbar}>
         <div className={styles.logo}>Logo</div>
         <div className={styles.links}>
             <h6>Home</h6>
-            <h6>About Us</h6>
-            <h6>Contact Us</h6>
+            <h6><Link href='/pages/about'>About us</Link></h6>
+            <h6><Link href='/pages/contact'>Contact us</Link></h6>
             <h6>Services</h6>
             <h6>Enquiry</h6>
         </div>
