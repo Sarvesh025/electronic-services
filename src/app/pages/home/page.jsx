c6c0ca2db0f00be1f80a3844dce17cebe440129d
Bibhuti Bhushan Saha <bibhuticodes@gmail.com>
2024-06-24 13:14:02 +0530

    implimented scroll to section

--- src/app/pages/home/page.jsx
+++ src/app/pages/home/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState} from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Navbar from '../../component/Navbar/page'
 import Services from '@/app/component/Services/page'
 import Whatsapp from '../../component/Whatsapp/page'
@@ -9,22 +9,32 @@ import CustomCarousel from '@/app/component/CustomCarousel/CustomCarousel'
 
 export default function Home() {
 
-        const [dataFromChild, setDataFromChild] = useState('');
-      
-        const handleDataFromChild = (data) => {
-          setDataFromChild(data);
-        };
-      
+    const [dataFromChild, setDataFromChild] = useState('');
+
+    let enquiryREF = useRef(null);
+
+    const handleClick = () => {
+        enquiryREF.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
+    const handleDataFromChild = (data) => {
+        setDataFromChild(data);
+    };
+
+   useEffect(() => {
+        
+   }, [])
+
     return (
         <div>
-            <Navbar/>
+            <Navbar scrollToForm={handleClick}/>
             <CustomCarousel />
-            <Services onData={handleDataFromChild} thumbnail='/ac-repair-service.jpg' title='Air Conditioner' description='sdjbnsdbdsyhvbyvbyuvbyuvbyvbuyvbsdyuhvbvysdbvybvwvbvywerbvyuhrbvyrhvbsdyhuvbsdhvsduyv uyhvrv wdvberv yvrvyerbvyre vyer vyer gre vyer gryv ereryg wev rey v eryb erv erg werg eruig erugrge rlg eragher ger gergr'/>
-            <Services onData={handleDataFromChild} thumbnail='/repairing-refrigerator.jpg' title='Refrigerator' description='sdjbnsdbdsyhvbyvbyuvbyuvbyvbuyvbsdyuhvbvysdbvybvwvbvywerbvyuhrbvyrhvbsdyhuvbsdhvsduyv uyhvrv wdvberv yvrvyerbvyre vyer vyer gre vyer gryv ereryg wev rey v eryb erv erg werg eruig erugrge rlg eragher ger gergr'/>
-            <Services onData={handleDataFromChild} thumbnail='/repairing-washing-machine.jpg' title='Washing Machine' description='sdjbnsdbdsyhvbyvbyuvbyuvbyvbuyvbsdyuhvbvysdbvybvwvbvywerbvyuhrbvyrhvbsdyhuvbsdhvsduyv uyhvrv wdvberv yvrvyerbvyre vyer vyer gre vyer gryv ereryg wev rey v eryb erv erg werg eruig erugrge rlg eragher ger gergr'/>
-            <Services onData={handleDataFromChild} thumbnail='/repair-microwave.jpg' title='Microwave' description='sdjbnsdbdsyhvbyvbyuvbyuvbyvbuyvbsdyuhvbvysdbvybvwvbvywerbvyuhrbvyrhvbsdyhuvbsdhvsduyv uyhvrv wdvberv yvrvyerbvyre vyer vyer gre vyer gryv ereryg wev rey v eryb erv erg werg eruig erugrge rlg eragher ger gergr'/>
-            <Form data = {dataFromChild}/>
-            <Whatsapp/> 
+            <Services scrollToForm={handleClick} onData={handleDataFromChild} thumbnail='/ac-repair-service.jpg' title='Air Conditioner' description='sdjbnsdbdsyhvbyvbyuvbyuvbyvbuyvbsdyuhvbvysdbvybvwvbvywerbvyuhrbvyrhvbsdyhuvbsdhvsduyv uyhvrv wdvberv yvrvyerbvyre vyer vyer gre vyer gryv ereryg wev rey v eryb erv erg werg eruig erugrge rlg eragher ger gergr' />
+            <Services scrollToForm={handleClick} onData={handleDataFromChild} thumbnail='/repairing-refrigerator.jpg' title='Refrigerator' description='sdjbnsdbdsyhvbyvbyuvbyuvbyvbuyvbsdyuhvbvysdbvybvwvbvywerbvyuhrbvyrhvbsdyhuvbsdhvsduyv uyhvrv wdvberv yvrvyerbvyre vyer vyer gre vyer gryv ereryg wev rey v eryb erv erg werg eruig erugrge rlg eragher ger gergr' />
+            <Services scrollToForm={handleClick} onData={handleDataFromChild} thumbnail='/repairing-washing-machine.jpg' title='Washing Machine' description='sdjbnsdbdsyhvbyvbyuvbyuvbyvbuyvbsdyuhvbvysdbvybvwvbvywerbvyuhrbvyrhvbsdyhuvbsdhvsduyv uyhvrv wdvberv yvrvyerbvyre vyer vyer gre vyer gryv ereryg wev rey v eryb erv erg werg eruig erugrge rlg eragher ger gergr' />
+            <Services scrollToForm={handleClick} onData={handleDataFromChild} thumbnail='/repair-microwave.jpg' title='Microwave' description='sdjbnsdbdsyhvbyvbyuvbyuvbyvbuyvbsdyuhvbvysdbvybvwvbvywerbvyuhrbvyrhvbsdyhuvbsdhvsduyv uyhvrv wdvberv yvrvyerbvyre vyer vyer gre vyer gryv ereryg wev rey v eryb erv erg werg eruig erugrge rlg eragher ger gergr' />
+            <Form data={dataFromChild} ref={enquiryREF} />
+            <Whatsapp />
             {/* <Contact /> */}
         </div>
     )
