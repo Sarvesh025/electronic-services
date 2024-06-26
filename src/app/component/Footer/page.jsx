d3732f256b9d56600f706ca32e3bbf6fcbf8c79d
Sarvesh025 <sarveshkumarroy1@gmail.com>
2024-06-26 15:05:48 +0530

    updated the footer

--- src/app/component/Footer/page.jsx
+++ src/app/component/Footer/page.jsx
@@ -2,33 +2,51 @@ import React from 'react'
 import styles from './footer.module.css'
 
 export default function Footer() {
-  return (
-    <div className={styles.footer}>
-        <div className={styles.top}>
-        <div className={styles.contact}>
-            <h2>CONTACT</h2>
-            <ul className={styles.info}>
-                <li><b>Phone</b> : +917890694858</li>
-                <li><b>Email</b> : starepairingcenter@gmail.com</li>
-                <li><b>Address</b> : hbsbc sdv dsfve eefe fwefe</li>
-            </ul>
-        </div>
-        <div className={styles.intro}>
-            <h2>STAR REPAIRING CENTER</h2>
-            <p>In our services, we prioritize customer satisfaction and strive to provide the best possible service to our customers. If you're in need of appliance repair services, don't hesitate to contact us. We're here to help you get your appliances up and running as soon as possible.</p>
-            <div className={styles.links}>
-                <h2>LINKS </h2>
-                <ul className={styles.link_info}>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact us</li>
-                </ul>
+
+    const contact = [{
+        title: 'Phone :',
+        value: '+917890694858'
+    },
+    {
+        title: 'Email :',
+        value: 'starepairingcenter@gmail.com'
+    },
+    {
+        title: 'Address :',
+        value: 'hbsbc sdv dsfve eefe fwefe'
+    }
+    ];
+
+    const intro = "In our services, we prioritize customer satisfaction and strive to provide the best possible service to our customers. If you're in need of appliance repair services, don't hesitate to contact us. We're here to help you get your appliances up and running as soon as possible."
+    return (
+        <div className={styles.footer}>
+            <div className={styles.top}>
+                <div className={styles.contact}>
+                    <h2>CONTACT</h2>
+                    <ul className={styles.info}>
+                        {contact.map((e, i) => {
+                            return (
+                                <li key={i}><b>{e.title}</b> {e.value}</li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div className={styles.intro}>
+                    <h2>STAR REPAIRING CENTER</h2>
+                    <p>{intro}</p>
+                    <div className={styles.links}>
+                        <h2>LINKS </h2>
+                        <ul className={styles.link_info}>
+                            <li>Home</li>
+                            <li>About us</li>
+                            <li>Contact us</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.copyright}>
+                &copy; All Rights Reserved.
             </div>
         </div>
-        </div>
-        <div className={styles.copyright}>
-           &copy; All Rights Reserved.
-        </div>
-    </div>
-  )
+    )
 }
